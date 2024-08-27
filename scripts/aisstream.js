0f001cc94b0c1405e7fa6e278ee0a21fe3bcdb9f
Nathan Griffiths <ngriffiths@postmedia.com>
2024-08-27 15:59:49 -0700

    aargh

--- scripts/aisstream.js
+++ scripts/aisstream.js
@@ -84,7 +84,7 @@ async function openWebSocket(url, apiKey) {
 	// on message rx
 	socket.addEventListener('message', (e) => {
 		let aisMessage = JSON.parse(e.data);
-		console.log(aisMessage)
+		// console.log(aisMessage)
 
 			// check for ships currently moored
 			if (aisMessage.MessageType === 'PositionReport') {
@@ -233,7 +233,7 @@ async function getShipStaticData(aisMessage) {
 		await saveData(shipsLookup, { filepath: ships_lookup_filepath, format: 'json', append: false });
 
 		// get ship details from Equasis
-		getShipDetails.init([data.ImoNumber]);
+		// getShipDetails.init([data.ImoNumber]);
 	} else {
 		// ship is cached remotely but not locally
 		if (!isLocalCache) {
