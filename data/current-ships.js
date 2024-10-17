398b9279750922915e1b3839b89bf2daf190c5a6
vs-postmedia <vs-postmedia@users.noreply.github.com>
2024-10-17 01:56:31 +0000

    Updated with latest data

--- data/current-ships.js
+++ data/current-ships.js
@@ -1,1 +1,1 @@
-export default [{"ImoNumber":9973834,"date":"2024-10-16"},{"ImoNumber":9877561,"date":"2024-10-16"},{"ImoNumber":9953054,"MMSI":538010361,"date":"2024-10-16"},{"ImoNumber":9953901,"date":"2024-10-16"}]
+export default [{"ImoNumber":9877561,"date":"2024-10-17"},{"ImoNumber":9953054,"date":"2024-10-17"},{"ImoNumber":9953901,"date":"2024-10-17"}]
