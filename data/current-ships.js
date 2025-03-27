62de590b08f2edcdcc7f448fc2a22e5ae52d0599
vs-postmedia <vs-postmedia@users.noreply.github.com>
2025-03-27 07:08:24 +0000

    Updated with latest data

--- data/current-ships.js
+++ data/current-ships.js
@@ -1,1 +1,1 @@
-export default [{"ImoNumber":9971719,"date":"2025-03-27"},{"ImoNumber":9893060,"date":"2025-03-27"},{"ImoNumber":9953042,"date":"2025-03-27"}]
+export default [{"ImoNumber":9893060,"date":"2025-03-27"},{"ImoNumber":9953042,"date":"2025-03-27"},{"ImoNumber":9971719,"date":"2025-03-27"}]
