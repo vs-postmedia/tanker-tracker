e07f03a83cd46bc61a72a5ad8a4f360af2d4e8db
vs-postmedia <vs-postmedia@users.noreply.github.com>
2026-02-17 10:30:04 +0000

    Updated with latest data

--- data/current-ships.js
+++ data/current-ships.js
@@ -1,1 +1,1 @@
-export default [{"Destination":"CHINA","ImoNumber":9976111,"timeArray":"2026,3,12,0,1"},{"Destination":"CAVAN","ImoNumber":9408310,"timeArray":"2026,2,14,2,0"},{"Destination":"VANCOUVER","ImoNumber":9893113,"timeArray":"2026,2,14,4,15"}]
+export default [{"Destination":"VANCOUVER","ImoNumber":9893113,"timeArray":"2026,2,14,4,15"},{"Destination":"CAVAN","ImoNumber":9408310,"timeArray":"2026,2,14,2,0"}]
