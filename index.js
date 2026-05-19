f0319d8c4e701a31799067ab0e9e60d71dab8a04
Nathan Griffiths <ngriffiths@postmedia.com>
2026-05-19 11:56:57 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-// import aisHub from './scripts/aishub.js';
 import aisStream from './scripts/aisstream.js';
 
 
@@ -7,15 +6,11 @@ import aisStream from './scripts/aisstream.js';
 const runtime = 50; // how long websocket will stay open, in minutes
 const ships_data_filepath = './data/ships-data';
 const aisWs = 'wss://stream.aisstream.io/v0/stream'; // AISStream websocket
-// const aishubUrl = 'https://data.aishub.net/ws.php'; // AISHub URL frag
-
-
 
 
 async function init() {
 	console.log(`Node version: ${process.version}`);
 	const aisApiKey = process.env.API_KEY_AISSTREAM;
-	// const aishubApiKey = process.env.API_KEY_AISHUB;
 	
 	// open stream
 	await aisStream.init(aisWs, aisApiKey, runtime);
